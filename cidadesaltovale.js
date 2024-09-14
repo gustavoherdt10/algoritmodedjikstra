feb43bdac7b4b3ff09879e3c9d1a0c17b2e8f3f4
Gustavo Herdt <98030680+gustavoherdt10@users.noreply.github.com>
2024-09-14 15:41:51 -0300

    envia

---
+++ cidadesaltovale.js
@@ -0,0 +1,18 @@
+// Grafo de representação das cidades e os valores de custo das rodovias
+const graph = {
+    'Ituporanga': { 'Aurora': 26, 'Atalanta': 33, 'Petrolandia': 18, 'Chapadao do Lageado': 26, 'Imbuia': 50, 'Vidal Ramos': 62 },
+    'Aurora': { 'Rio do Sul': 26, 'Ituporanga': 26, 'Atalanta': 59.4 },
+    'Rio do Sul': { 'Aurora': 26, 'Lontras': 39, 'Ibirama': 81, 'Laurentino': 39, 'Trombudo Central': 63 },
+    'Agronomica': { 'Trombudo Central': 54, 'Presidente Getulio': 108 },
+    'Trombudo Central': { 'Atalanta': 138.6, 'Rio do Sul': 63, 'Laurentino': 57, 'Agronomica': 54, 'Agrolandia': 21 },
+    'Agrolandia': { 'Trombudo Central': 21 },
+    'Atalanta': { 'Ituporanga': 33, 'Aurora': 59.4, 'Trombudo Central': 138.6 },
+    'Petrolandia': { 'Ituporanga': 18, 'Chapadao do Lageado': 50.6 },
+    'Chapadao do Lageado': { 'Ituporanga': 26, 'Petrolandia': 50.6, 'Imbuia': 25 },
+    'Imbuia': { 'Ituporanga': 50, 'Chapadao do Lageado': 25 },
+    'Vidal Ramos': { 'Ituporanga': 62, 'Ibirama': 299.2 },
+    'Lontras': { 'Rio do Sul': 39, 'Ibirama': 51, 'Presidente Getulio': 58 },
+    'Ibirama': { 'Rio do Sul': 81, 'Vidal Ramos': 299.2, 'Lontras': 51, 'Presidente Getulio': 36 },
+    'Presidente Getulio': { 'Agronomica': 108, 'Lontras': 58, 'Ibirama': 36, 'Laurentino': 114 },
+    'Laurentino': { 'Rio do Sul': 39, 'Trombudo Central': 57, 'Presidente Getulio': 114 }
+};
